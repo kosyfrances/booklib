212bd7a1faf21fb00fbd92f7d58ff2d9ccafec85
Kosy Anyanwu <kosy.anyanwu@andela.com>
2017-02-27 16:43:07 +0100

    Hook up templates and book app with home page url

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomeViewTest(TestCase):
+    """Tests for Home View."""
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_home_page_route_is_successful(self):
+        response = self.client.get(reverse('books:home'))
+
+        self.assertEqual(response.status_code, 200)
